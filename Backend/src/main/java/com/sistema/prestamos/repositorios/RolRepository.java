0d4e034a669a7964caacc82e20ffd0ab1e3f114b
Matias Cayetano <134734889+jmzcayetano@users.noreply.github.com>
2024-04-12 16:12:22 -0500

    Delete Backend directory

--- Backend/src/main/java/com/sistema/prestamos/repositorios/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sistema.prestamos.repositorios;
-
-import com.sistema.prestamos.modelo.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol,Long> {
-}
