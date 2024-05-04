c6756ea007abcd5dd0485769c71308a1947752cd
PatrickG2003 <patrickgr26112003@gmail.com>
2024-05-04 18:40:26 -0500

    Combos con respectivo Sector

--- src/main/java/pe/edu/cibertec/dao/SectorRepository.java
+++ src/main/java/pe/edu/cibertec/dao/SectorRepository.java
@@ -1,7 +1,20 @@
 package pe.edu.cibertec.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import pe.edu.cibertec.entity.Sector;
+import pe.edu.cibertec.entity.Solicitud;
 
 public interface SectorRepository extends JpaRepository<Sector, Integer> {
+	
+	
+	@Query("select s from Sector s where s.idsectorsuperior=?1 ")
+	public List<Sector> listaSectorParaPrestamista(int d);
+	
+	@Query("select s from Sector s where s.idsectorsuperior=0 ")
+	public List<Sector> listaSectorParaJefes();
+	
 }
