486bcf5f6e58ab320c41291ee2fa67cf67b8b619
mdelacruzg <maycol.delacruz.g@gmail.com>
2024-05-04 23:56:03 -0500

    limpieza de codigo

--- src/main/java/pe/edu/cibertec/controller/PrestatarioController.java
+++ src/main/java/pe/edu/cibertec/controller/PrestatarioController.java
@@ -105,20 +105,15 @@ public class PrestatarioController {
 	public String listarSolicitudes(Model model) {
 		// Obtener la autenticación del contexto de seguridad
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-		// Comprobar si el principal es una instancia de UserDetails
 		if (auth != null && auth.getPrincipal() instanceof UserDetails) {
 			UserDetails userDetails = (UserDetails) auth.getPrincipal();
 			String username = userDetails.getUsername();
 
-			// Utilizar el servicio de Usuario para obtener el usuario por nombre de usuario
 			Usuario usuario = servicioUsu.sesionUsuario(username);
 
-			// Comprobar si el usuario no es null
 			if (usuario != null) {
 				int usuarioId = usuario.getId();
 
-				// Obtener las solicitudes para este usuario
 				model.addAttribute("Solicitud", serviceSolicitud.listaSolicitudesPorUsuario(usuarioId));
 			} else {
 				model.addAttribute("error", "Usuario no encontrado");
