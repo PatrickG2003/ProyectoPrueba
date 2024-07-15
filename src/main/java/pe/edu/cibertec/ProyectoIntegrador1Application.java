6c17609bb170e5b2cf11bc5eafd0b1d1174567f0
PatrickG2003 <patrickgr26112003@gmail.com>
2024-07-15 18:51:01 -0500

    Cambio de properties para la bd

--- src/main/java/pe/edu/cibertec/ProyectoIntegrador1Application.java
+++ src/main/java/pe/edu/cibertec/ProyectoIntegrador1Application.java
@@ -2,6 +2,10 @@ package pe.edu.cibertec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoIntegrador1Application {
@@ -9,5 +13,21 @@ public class ProyectoIntegrador1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoIntegrador1Application.class, args);
 	}
+	
+	
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
+	
 
 }
